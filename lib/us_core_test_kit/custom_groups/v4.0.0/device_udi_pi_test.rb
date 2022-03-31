0f06299c18b59647a918b5a5e9a0fb0a4ae09fab
Yunwei Wang <yunwwang@gmail.com>
2022-03-31 13:45:04 -0500

    add UDI-PI testing

---
+++ lib/us_core_test_kit/custom_groups/v4.0.0/device_udi_pi_test.rb
@@ -0,0 +1,42 @@
+module USCoreTestKit
+  module USCoreV400
+    class DeviceUdiPiTest < Inferno::Test
+      id :us_core_v400_device_udi_pi_test
+      title 'Device resources returned during previous tests have UDI-PI elements'
+      description %(
+        This test verifies implantable medical devices that have UDI information represent all of the UDI-PI elements that are present in the UDI code
+        in the corresponding US Core Implantable Device Profile element.
+      )
+
+      def scratch_resources
+        scratch[:device] ||= {}
+      end
+
+      run do
+        resources = scratch_resources[:all] || []
+
+        skip_if resources.blank?,
+                'No Devices resources appeart to be available. ' \
+                'Please use patients with more information.'
+
+        resources.each do |device|
+          if device.udiCarrier&.first&.carrierHRF.present? &&
+            !(
+              device.manufactureDate.present? ||
+              device.expirationDate.present? ||
+              device.lotNumber.present? ||
+              device.serialNumber.present? ||
+              device.distinctIdentifier.present?
+            )
+            messages << {
+              type: 'error',
+              message: "Device/#{device.id} has UDI information but does not have any UDI-PI elements presented"
+            }
+          end
+        end
+
+        assert !messages.any?, "Resource does not have DocumentReference.custodian, Provenance.agent.who, nor Provenance.agent.onBehalfOf"
+      end
+    end
+  end
+end
