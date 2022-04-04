094f0758796f0bfbceec9d9e1d8e44d4a880b47d
Stephen MacVicar <smacvicar@mitre.org>
2022-04-04 10:33:25 -0400

    add specs for resolving references with a target profile

--- spec/us_core/reference_resolution_test_spec.rb
+++ spec/us_core/reference_resolution_test_spec.rb
@@ -143,5 +143,52 @@ RSpec.describe USCoreTestKit::ReferenceResolutionTest do
         expect(test.requests.length).to eq(1)
       end
     end
+
+    context 'with a target profile' do
+      context 'with a contained reference' do
+        let(:reference_string) { '#123' }
+        let(:contained_resource) { FHIR::Encounter.new(id: '123') }
+        let(:resource) { FHIR::Observation.new(encounter: { reference: reference_string }, contained: [contained_resource]) }
+        let(:reference) { resource.encounter }
+
+        it 'returns true if the contained resource conforms to the target profile' do
+          allow(test).to receive(:resource_is_valid?).with(resource: contained_resource, profile_url: 'PROFILE').and_return(true)
+
+          expect(test.validate_reference_resolution(resource, reference, 'PROFILE')).to be(true)
+        end
+
+        it 'returns false if the contained resource does not conform to the target profile' do
+          allow(test).to receive(:resource_is_valid?).with(resource: contained_resource, profile_url: 'PROFILE').and_return(false)
+
+          expect(test.validate_reference_resolution(resource, reference, 'PROFILE')).to be(false)
+        end
+      end
+
+      context 'with a non-contained reference' do
+        let(:reference_string) { 'Encounter/123' }
+        let(:resource) do
+          FHIR::Observation.new(encounter: { reference: reference_string })
+        end
+        let(:reference) { resource.encounter }
+        let(:referenced_resource) { FHIR::Encounter.new(id: '123') }
+
+        before do
+          stub_request(:get, "#{base_url}/#{reference_string}")
+            .to_return(status: 200, body: referenced_resource.to_json)
+        end
+
+        it 'returns true if the referenced resource conforms to the target profile' do
+          allow(test).to receive(:resource_is_valid?).with(resource: referenced_resource, profile_url: 'PROFILE').and_return(true)
+
+          expect(test.validate_reference_resolution(resource, reference, 'PROFILE')).to be(true)
+        end
+
+        it 'returns false if the referenced resource does not conform to the target profile' do
+          allow(test).to receive(:resource_is_valid?).with(resource: referenced_resource, profile_url: 'PROFILE').and_return(false)
+
+          expect(test.validate_reference_resolution(resource, reference, 'PROFILE')).to be(false)
+        end
+      end
+    end
   end
 end
