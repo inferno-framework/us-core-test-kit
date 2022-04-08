d78fad8c294295844eaa23ab5332bf20e67b4c9c
Yunwei Wang <yunwwang@gmail.com>
2022-04-08 16:41:32 -0500

    Add must_support spec tests

--- spec/us_core/must_support_test_spec.rb
+++ spec/us_core/must_support_test_spec.rb
@@ -2,8 +2,7 @@ RSpec.describe USCoreTestKit::MustSupportTest do
   let(:suite) { Inferno::Repositories::TestSuites.new.find('us_core_v400') }
   let(:session_data_repo) { Inferno::Repositories::SessionData.new }
   let(:test_session) { repo_create(:test_session, test_suite_id: suite.id) }
-  let(:url) { 'http://example.com/fhir' }
-  let(:error_outcome) { FHIR::OperationOutcome.new(issue: [{ severity: 'error' }]) }
+  let(:patient_ref) { 'Patient/85' }
 
   def run(runnable, inputs = {})
     test_run_params = { test_session_id: test_session.id }.merge(runnable.reference_hash)
@@ -19,49 +18,11 @@ RSpec.describe USCoreTestKit::MustSupportTest do
     Inferno::TestRunner.new(test_session: test_session, test_run: test_run).run(runnable)
   end
 
-  describe 'must support test with choice elements' do
-    let(:must_support_test) do
-      Class.new(Inferno::Test) do
-        include USCoreTestKit::MustSupportTest
-
-        def self.metadata
-          @metadata ||=
-            USCoreTestKit::Generator::GroupMetadata.new(
-              YAML.load_file(
-                File.join(
-                  __dir__,
-                  '..',
-                  'fixtures',
-                  'device_metadata.yml'
-                )
-              )
-            )
-        end
-
-        def resource_type
-          'Device'
-        end
-
-        def scratch_resources
-          scratch[:device] ||= {}
-        end
-
-        fhir_client { url :url }
-        input :url
-
-        run do
-          perform_must_support_test(all_scratch_resources)
-        end
-      end
-    end
-    let(:patient_ref) { 'Patient/85' }
+  describe 'must support test for choice elements and regular elements' do
+    let(:device_must_support_test) { Inferno::Repositories::Tests.new.find('us_core_v311_device_must_support_test')}
     let(:device) do
       FHIR::Device.new(
-        udiCarrier: [
-          {
-              deviceIdentifier: '43069338026389'
-          }
-        ],
+        udiCarrier: [{ deviceIdentifier: '43069338026389', carrierHRF: 'carrierHRF' }],
         distinctIdentifier: '43069338026389',
         manufactureDate: '2000-03-02T18:33:18-05:00',
         expirationDate: '2025-03-17T19:33:18-04:00',
@@ -76,34 +37,161 @@ RSpec.describe USCoreTestKit::MustSupportTest do
       )
     end
 
-    before do
-      Inferno::Repositories::Tests.new.insert(must_support_test)
-    end
-
     it 'fails if server supports none of the choice' do
-      allow_any_instance_of(must_support_test)
+      device.udiCarrier.first.carrierHRF = nil
+      allow_any_instance_of(device_must_support_test)
         .to receive(:scratch_resources).and_return(
-              {
-                all: [device]
-              }
-            )
+          {
+            all: [device]
+          }
+        )
 
-      result = run(must_support_test, url: url)
+      result = run(device_must_support_test)
       expect(result.result).to eq('skip')
       expect(result.result_message).to include('udiCarrier.carrierAIDC, udiCarrier.carrierHRF')
     end
 
     it 'passes if server supports at least one of the choice' do
-      device.udiCarrier.first.carrierHRF = '(01)43069338026389(11)000302(17)250317(10)1134(21)842026117977'
-      allow_any_instance_of(must_support_test)
+      allow_any_instance_of(device_must_support_test)
         .to receive(:scratch_resources).and_return(
-              {
-                all: [device]
-              }
-            )
+          {
+            all: [device]
+          }
+        )
 
-      result = run(must_support_test, url: url)
+      result = run(device_must_support_test)
       expect(result.result).to eq('pass')
+    end
+
+    it 'fails if server does not support one MS element' do
+      device.distinctIdentifier = nil
+      allow_any_instance_of(device_must_support_test)
+        .to receive(:scratch_resources).and_return(
+          {
+            all: [device]
+          }
+        )
+
+      result = run(device_must_support_test)
+      expect(result.result).to eq('skip')
+      expect(result.result_message).to include('distinctIdentifier')
+    end
+  end
+
+  describe 'must support test for extensions' do
+    let(:patient_must_support_test) { Inferno::Repositories::Tests.new.find('us_core_v311_patient_must_support_test')}
+    let(:patient) do
+      FHIR::Patient.new(
+        identifier: [{system: 'system', value: 'value'}],
+        name: [{family: 'family', given: 'given'}],
+        telecom: [{system: 'phone', value: 'value', use: 'home'}],
+        gender: 'male',
+        birthDate: '2020-01-01',
+        address: [{line: 'line', city: 'city', state: 'state', postalCode: 'postalCode', period: {start: '2020-01-01'}}],
+        communication: [{language: {text: 'text'}}],
+        extension: [
+          {
+            url: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-race',
+            extension: [
+              {url: 'ombCategory', valueCoding: {display: 'display'}},
+              {url: 'text', valueString: 'valueString'}
+            ]
+          },
+          {
+            url: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-ethnicity',
+            extension: [
+              {url: 'ombCategory', valueCoding: {display: 'display'}},
+              {url: 'text', valueString: 'valueString'}
+            ]
+          },
+          {
+            url: 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-birthsex',
+            valueCode: 'M'
+          }
+        ]
+      )
+    end
+
+    it 'passes if server suports all MS extensions' do
+      allow_any_instance_of(patient_must_support_test)
+        .to receive(:scratch_resources).and_return(
+          {
+            all: [patient]
+          }
+        )
+
+      result = run(patient_must_support_test)
+
+      expect(result.result).to eq('pass')
+    end
+
+    it 'fails if server does not suport one MS extensions' do
+      patient.extension.delete_at(0)
+
+      allow_any_instance_of(patient_must_support_test)
+        .to receive(:scratch_resources).and_return(
+          {
+            all: [patient]
+          }
+        )
+
+      result = run(patient_must_support_test)
+
+      expect(result.result).to eq('skip')
+      expect(result.result_message).to include('Patient.extension:race')
+    end
+  end
+
+  describe 'must support test for slices' do
+    let(:care_plan_must_support_test) { Inferno::Repositories::Tests.new.find('us_core_v311_care_plan_must_support_test')}
+    let(:careplan) do
+      FHIR::CarePlan.new(
+        text: { status: 'status' },
+        status: 'status',
+        intent: 'intent',
+        category: [
+          {
+            coding: [
+              {
+                system: 'http://hl7.org/fhir/us/core/CodeSystem/careplan-category',
+                code: 'assess-plan'
+              }
+            ]
+          }
+        ],
+        subject: {
+          reference: patient_ref
+        }
+      )
+    end
+
+    it 'passes if server suports all MS slices' do
+      allow_any_instance_of(care_plan_must_support_test)
+        .to receive(:scratch_resources).and_return(
+          {
+            all: [careplan]
+          }
+        )
+
+
+      result = run(care_plan_must_support_test)
+
+      expect(result.result).to eq('pass')
+    end
+
+    it 'fails if server does not suport one MS extensions' do
+      careplan.category.first.coding.first.code = 'something else'
+      allow_any_instance_of(care_plan_must_support_test)
+        .to receive(:scratch_resources).and_return(
+          {
+            all: [careplan]
+          }
+        )
+
+      result = run(care_plan_must_support_test)
+
+      expect(result.result).to eq('skip')
+      expect(result.result_message).to include('CarePlan.category:AssessPlan')
     end
   end
 end
