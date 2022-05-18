0ebeecf51fbf1b0f9c09d7cdcc3cdb6ca116e7de
Yunwei Wang <yunwwang@gmail.com>
2022-05-18 09:11:19 -0500

    limit data precision to v4.0.0 and above

--- lib/us_core_test_kit/search_test.rb
+++ lib/us_core_test_kit/search_test.rb
@@ -568,7 +568,9 @@ module USCoreTestKit
           when FHIR::Address
             element.text || element.city || element.state || element.postalCode || element.country
           else
-            if metadata.search_definitions[name.to_sym][:type] == 'date' && params_with_comparators&.include?(name)
+            if metadata.version != 'v3.1.1' &&
+               metadata.search_definitions[name.to_sym][:type] == 'date' &&
+               params_with_comparators&.include?(name)
               # convert date search to greath-than comparator search with correct precision
               # For all date search parameters:
               #   Patient.birthDate does not mandate comparators so cannot be converted
@@ -576,7 +578,7 @@ module USCoreTestKit
               #   All others have second + time offset precision
               if /^\d{4}$/.match?(element) || # YYYY
                  /^\d{4}-\d{2}$/.match?(element) || # YYYY-MM
-                (/^\d{4}-\d{2}-\d{2}$/.match?(element) && resource_type != "Goal") # YYYY-MM-DD
+                (/^\d{4}-\d{2}-\d{2}$/.match?(element) && resource_type != "Goal") # YYYY-MM-DD AND Resource is NOT Goal
                 "gt#{(DateTime.xmlschema(element)-1).xmlschema}"
               else
                 element
