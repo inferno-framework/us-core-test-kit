f53ad2b0931dcf5b47f3fc3f73d1ccfda03bb85f
Yunwei Wang <yunwwang@gmail.com>
2022-07-14 13:45:14 -0500

    Rearrange search sequence for Observation imaging/clinical-test/survey

--- lib/us_core_test_kit/generated/v5.0.1/observation_imaging/observation_imaging_patient_code_search_test.rb
+++ lib/us_core_test_kit/generated/v5.0.1/observation_imaging/observation_imaging_patient_code_search_test.rb
@@ -13,19 +13,6 @@ patient + code on the Observation resource. This test
 will pass if resources are returned and match the search criteria. If
 none are returned, the test is skipped.
 
-This test verifies that the server supports searching by reference using
-the form `patient=[id]` as well as `patient=Patient/[id]`. The two
-different forms are expected to return the same number of results. US
-Core requires that both forms are supported by US Core responders.
-
-Because this is the first search of the sequence, resources in the
-response will be used for subsequent tests.
-
-Additionally, this test will check that GET and POST search methods
-return the same number of results. Search by POST is required by the
-FHIR R4 specification, and these tests interpret search by GET as a
-requirement of US Core v5.0.1.
-
 [US Core Server CapabilityStatement](http://hl7.org/fhir/us/core//CapabilityStatement-us-core-server.html)
 
       )
@@ -37,14 +24,10 @@ requirement of US Core v5.0.1.
   
       def self.properties
         @properties ||= SearchTestProperties.new(
-          first_search: true,
-        fixed_value_search: true,
-        resource_type: 'Observation',
+          resource_type: 'Observation',
         search_param_names: ['patient', 'code'],
         possible_status_search: true,
-        token_search_params: ['code'],
-        test_reference_variants: true,
-        test_post_search: true
+        token_search_params: ['code']
         )
       end
 
