a6bb36f6b1f5d3acaf585f35d024f2183efca8af
Stephen MacVicar <smacvicar@mitre.org>
2022-11-03 09:19:58 -0400

    regenerate tests

--- lib/us_core_test_kit/generated/v4.0.0/us_core_test_suite.rb
+++ lib/us_core_test_kit/generated/v4.0.0/us_core_test_suite.rb
@@ -39,6 +39,19 @@ module USCoreTestKit
   module USCoreV400
     class USCoreTestSuite < Inferno::TestSuite
       title 'US Core v4.0.0'
+      description %(
+        The US Core Test Kit tests systems for their conformance to the [US Core
+        Implementation Guide](http://hl7.org/fhir/us/core/STU4).
+
+        Resources are validated with the FHIR Java validator using `tx.fhir.org`
+        as the terminology server. Users should note that the although the ONC
+        Certification (g)(10) Standardized API Test Suite includes tests from
+        this suite, [it uses a different method to perform terminology
+        validation](https://github.com/onc-healthit/onc-certification-g10-test-kit/wiki/FAQ#q-why-do-some-resources-fail-in-us-core-test-kit-with-terminology-validation-errors).
+        As a result, resource validation results may not be consistent between
+        the US Core Test Suite and the ONC Certification (g)(10) Standardized
+        API Test Suite.
+      )
       version VERSION
 
       VALIDATION_MESSAGE_FILTERS = [
