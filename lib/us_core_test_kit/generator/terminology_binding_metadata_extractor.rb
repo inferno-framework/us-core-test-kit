eb3e9a57cfbd3d6e0d3481666ab33f98f36c701b
yunwwang <yunwwang@gmail.com>
2022-12-01 08:08:10 -0600

    FI-1792 Generator handle required binding slice (#94)
    * remove non-required binding from metadata
    
    * add required_binding_slice to bindind definition
    
    * Remove optional binding slice from binding definition
    
    * typo
    
    * add ? for a method
    
    Co-authored-by: Yunwei Wang <>

--- lib/us_core_test_kit/generator/terminology_binding_metadata_extractor.rb
+++ lib/us_core_test_kit/generator/terminology_binding_metadata_extractor.rb
@@ -27,21 +27,33 @@ module USCoreTestKit
         end
       end
 
+      def element_has_optional_binding_slice?(element)
+        element.sliceName.present? && element.min == 0
+      end
+
       def profile_elements_with_bindings
         profile_elements
-          .select { |element| element.binding.present? }
-          .reject { |element| element_has_fixed_value? element }
+          .select { |element| element.binding.present? && element.binding.strength == 'required'}
+          .reject { |element| element_has_fixed_value?(element) || element_has_optional_binding_slice?(element) }
       end
+
+
 
       def element_terminology_bindings
         profile_elements_with_bindings.map do |element|
-          {
+          binding = {
             type: element.type.first.code,
             strength: element.binding.strength,
             # Goal.target.detail has an unbound binding
             system: element.binding.valueSet&.split('|')&.first,
             path: element.path.gsub('[x]', '').gsub("#{resource}.", '')
           }
+
+          if element.sliceName.present? && element.min > 0
+            binding[:required_binding_slice] = true
+          end
+
+          binding
         end
       end
 
