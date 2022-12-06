489c93714dffee9b2048a5e77a95519ff48779b3
Yunwei Wang <>
2022-12-06 09:49:00 -0600

    update unit test

--- spec/us_core/provenance_validator_spec.rb
+++ spec/us_core/provenance_validator_spec.rb
@@ -1,9 +1,12 @@
 require_relative '../../lib/us_core_test_kit/provenance_validator'
 
 RSpec.describe USCoreTestKit::ProvenanceValidator do
+  let(:provenance_id){'123'}
+
   describe '.validate' do
     it 'passes when agent.who is a Practitioner and agent.onBehalfOf exists' do
       resource = FHIR::Provenance.new(
+        id: provenance_id,
         agent:[
           {
             who: {
@@ -24,6 +27,7 @@ RSpec.describe USCoreTestKit::ProvenanceValidator do
 
     it 'passes when agent.who is a Device and agent.onBehalfOf exists' do
       resource = FHIR::Provenance.new(
+        id: provenance_id,
          agent:[
            {
              who: {
@@ -44,7 +48,8 @@ RSpec.describe USCoreTestKit::ProvenanceValidator do
 
     it 'passes when agent.who is not a Practitioner nor Device and agent.onBehalfOf does not exists' do
       resource = FHIR::Provenance.new(
-         agent:[
+        id: provenance_id,
+        agent:[
            {
              who: {
                reference: 'Organization/1'
@@ -61,7 +66,8 @@ RSpec.describe USCoreTestKit::ProvenanceValidator do
 
     it 'fails when agent.who is a Practitioner and agent.onBehalfOf does not exists' do
       resource = FHIR::Provenance.new(
-         agent:[
+        id: provenance_id,
+        agent:[
            {
              who: {
                reference: 'Practitioner/1'
@@ -75,12 +81,12 @@ RSpec.describe USCoreTestKit::ProvenanceValidator do
       expect(result).to be_an(Array)
       expect(result.length).to eq(1)
       expect(result.first[:type]).to eq('error')
-      expect(result.first[:message]).to include('Rule provenance-1')
+      expect(result.first[:message]).to start_with("Provenance/#{provenance_id}: Rule provenance-1")
     end
 
     it 'fails when agent.who is a Device and agent.onBehalfOf does not exists' do
       resource = FHIR::Provenance.new(
-         id: '1',
+        id: provenance_id,
          agent:[
            {
              who: {
@@ -95,7 +101,7 @@ RSpec.describe USCoreTestKit::ProvenanceValidator do
       expect(result).to be_an(Array)
       expect(result.length).to eq(1)
       expect(result.first[:type]).to eq('error')
-      expect(result.first[:message]).to start_with('Provenance/1: Rule provenance-1')
+      expect(result.first[:message]).to start_with("Provenance/#{provenance_id}: Rule provenance-1")
     end
   end
 end
