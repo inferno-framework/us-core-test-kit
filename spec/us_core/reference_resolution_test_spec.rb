e91e0574d75e9b7cf16a81da3ce59a1b91ddd55b
Yunwei Wang <>
2022-12-08 11:00:56 -0600

    remove unused variable

--- spec/us_core/reference_resolution_test_spec.rb
+++ spec/us_core/reference_resolution_test_spec.rb
@@ -19,7 +19,6 @@ RSpec.describe USCoreTestKit::ReferenceResolutionTest do
         FHIR::Observation.new(encounter: { reference: reference_string })
       end
       let(:reference) { resource.encounter }
-      let(:referenced_resource) { FHIR::Encounter.new(id: '123') }
 
       it 'returns true' do
         target_profile = 'abc'
