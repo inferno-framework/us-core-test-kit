b8df232ba2933018522bf0668bc396b8bc210223
Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
2023-02-02 07:58:48 -0500

    FI-1888: Update profile support test (#103)
    * remove profile checks and add optional resource check to profile support test
    
    * add specs for profile support test

--- lib/us_core_test_kit/custom_groups/capability_statement/profile_support_test.rb
+++ lib/us_core_test_kit/custom_groups/capability_statement/profile_support_test.rb
@@ -9,7 +9,9 @@ module USCoreTestKit
       The US Core Server SHALL:
       1. Support the US Core Patient resource profile.
       2. Support at least one additional resource profile from the list of US
-          Core Profiles.
+         Core Profiles.
+
+      In order to support USCDI, servers must support all USCDI resources.
       ```
     )
     uses_request :capability_statement
@@ -23,30 +25,25 @@ module USCoreTestKit
           &.each_with_object([]) do |rest, resources|
             rest.resource.each { |resource| resources << resource.type }
           end.uniq
-      supported_profiles =
-        capability_statement.rest
-          &.flat_map(&:resource)
-          &.flat_map { |resource| resource.supportedProfile + [resource.profile] }
-          &.compact || []
-      supported_profiles.map! { |profile_url| profile_url.split('|').first }
 
       assert supported_resources.include?('Patient'), 'US Core Patient profile not supported'
 
-      target_profiles = config.options[:target_profiles]
+      us_core_resources = config.options[:us_core_resources]
 
-      other_resources = target_profiles.keys.reject { |resource_type| resource_type == 'Patient' }
+      other_resources = us_core_resources.reject { |resource_type| resource_type == 'Patient' }
       other_resources_supported = other_resources.any? { |resource| supported_resources.include? resource }
       assert other_resources_supported, 'No US Core resources other than Patient are supported'
 
-      target_profiles.each do |resource_type, profiles|
-        next unless supported_resources.include? resource_type
+      if config.options[:required_resources].present?
+        missing_resources = config.options[:required_resources] - supported_resources
 
-        profiles.each do |profile|
-          warning do
-            assert supported_profiles&.include?(profile),
-                    "CapabilityStatement does not claim support for US Core #{resource_type} profile: #{profile}"
-          end
-        end
+        missing_resource_list =
+          missing_resources
+          .map { |resource| "`#{resource}`" }
+          .join(', ')
+
+        assert missing_resources.empty?,
+               "The CapabilityStatement did not list support for the following resources: #{missing_resource_list}"
       end
     end
   end
