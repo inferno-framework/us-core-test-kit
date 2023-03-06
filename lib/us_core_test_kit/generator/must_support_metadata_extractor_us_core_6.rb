f14721dbb49915b8ecd089d2dab3a34c86f30193
yunwwang <yunwwang@gmail.com>
2023-03-06 13:23:04 -0600

    FI-1814 SimpleObservation.category valueset expansion (#112)
    * working on observation-occupation
    
    * remove package
    
    * generate first time
    
    * generate first time
    
    * Move previous igs back
    
    * Add MustSupport Choice and Patient USCDI elements
    
    * Hide v6.0.0-ballot test suite
    
    * rename preset. restore v6.0.0-ballot in us_core_test_kit
    
    * rebase for reference generation bug
    
    * FI-1841 Add MedicationRequest USCDI and choice
    
    * Run generator again
    
    * add uscdi_only to reason[x] choice
    
    * remove generated test
    
    * re generate tests
    
    * Add values for ServiceRequest.category and SimpleObservation.category
    
    * FI-1847 Fix values for ScreeningAssessment.category
    
    * FI-1825 Fix ObservationOccuption.component:industry slicing
    
    ---------
    
    Co-authored-by: Yunwei Wang <>

--- lib/us_core_test_kit/generator/must_support_metadata_extractor_us_core_6.rb
+++ lib/us_core_test_kit/generator/must_support_metadata_extractor_us_core_6.rb
@@ -5,6 +5,8 @@ module USCoreTestKit
     class MustSupportMetadataExtractorUsCore6
       attr_accessor :profile, :must_supports
 
+      US_CORE_CATEGORY = ['sdoh', 'functional-status', 'disability-status', 'cognitive-status']
+
       def initialize(profile, must_supports)
         self.profile = profile
         self.must_supports = must_supports
@@ -18,6 +20,10 @@ module USCoreTestKit
         add_must_support_choices
         add_uscdi_elements
         add_value_set_expansion
+        # TODO: US Core 6.0.0-ballot version has a bug in Observation Occupation Profile.
+        # Slicing on Observation.component:industry is not correct. See HL7 JIRA FHIR-39608
+        # This is a temparory fix. This method SHALL be removed after US Core 6.0.0 release
+        replace_occupation_industry
       end
 
       def add_must_support_choices
@@ -80,7 +86,63 @@ module USCoreTestKit
       end
 
       def add_value_set_expansion
-        us_core_5_extractor.add_value_set_expansion
+        us_core_5_extractor.add_document_reference_category_values
+        add_observation_screening_assessment_values
+        add_service_request_category_values
+        add_simple_observation_category_value
+      end
+
+      def add_observation_screening_assessment_values
+        return unless profile.url == 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-screening-assessment'
+
+        slice = must_supports[:slices].find{|slice| slice[:name] == 'Observation.category:uscore'}
+
+        if slice.present?
+          slice[:discriminator][:values] = US_CORE_CATEGORY
+        end
+      end
+
+      def add_service_request_category_values
+        return unless profile.type == 'ServiceRequest'
+
+        slice = must_supports[:slices].find{|slice| slice[:path] == 'category'}
+
+        if slice.present?
+          slice[:discriminator][:values]
+            .concat(US_CORE_CATEGORY)
+            .concat(['108252007', '363679005', '409063005', '409073007', '387713003'])
+        end
+      end
+
+      def add_simple_observation_category_value
+        return unless profile.url == 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-simple-observation'
+
+        slice = must_supports[:slices].find{|slice| slice[:path] == 'category'}
+
+        if slice.present?
+          slice[:discriminator][:values].concat(US_CORE_CATEGORY)
+        end
+      end
+
+      def replace_occupation_industry
+        # TODO: US Core 6.0.0-ballot version has a bug in Observation Occupation Profile.
+        # Slicing on Observation.component:industry is not correct. See HL7 JIRA FHIR-39608
+        # This is a temparory fix. This method SHALL be removed after US Core 6.0.0 release
+
+        return unless profile.url == 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-occupation'
+
+        must_supports[:slices].delete_if { |slice| slice[:name].include?('Observation.component:industry') }
+
+        must_supports[:slices] << {
+          name: 'Observation.component:industry',
+          path: 'component.code',
+          discriminator: {
+            type: 'patternCodeableConcept',
+            path: '',
+            code: '86188-0',
+            system: 'http://loinc.org'
+          }
+        }
       end
     end
   end
