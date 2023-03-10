f2894a6a368ede15213b6584b6879b9af7f7e262
Yunwei Wang <>
2023-03-10 12:56:03 -0600

    generate search metadata for extension

--- lib/us_core_test_kit/generator/search_definition_metadata_extractor.rb
+++ lib/us_core_test_kit/generator/search_definition_metadata_extractor.rb
@@ -16,6 +16,7 @@ module USCoreTestKit
         @search_definition ||=
           {
             elements: elements,
+            extensions: extensions,
             full_paths: full_paths,
             comparators: comparators,
             values: values,
@@ -42,28 +43,67 @@ module USCoreTestKit
             path.scan(/[. ]as[( ]([^)]*)[)]?/).flatten.map do |as_type|
               path.gsub!(/[. ]as[( ](#{as_type}[^)]*)[)]?/, as_type.upcase_first) if as_type.present?
             end
-            path.split('|')
+
+            full_paths = path.split('|')
+
+            #require 'pry'; require 'pry-byebug'; binding.pry if name == 'asserted-date'
+
+            #there is a bug in US Core 5 asserted-date search parameter. See FHIR-40573
+            if param.version == '5.0.1' && name == 'asserted-date'
+              remove_additional_extension_from_asserted_date(full_paths)
+            end
+
+            full_paths
           end
       end
 
-      def elements
-        @elements ||= full_paths.map do |a_path|
-          a_path.include?('extension.where') ? '' : a_path.gsub("#{resource}.", '')
-        end.compact
+      def remove_additional_extension_from_asserted_date(full_paths)
+        full_paths.each do |full_path|
+          next unless full_path.include?('http://hl7.org/fhir/StructureDefinition/condition-assertedDate')
+          full_path.gsub!(/\).extension./, ').')
+        end
       end
+
+      def elements
+        @elements ||= full_paths.select { |a_path| !a_path.include?('extension.where') }
+                                .map { |a_path| a_path.gsub("#{resource}.", '') }
+      end
+
+      def extensions
+        @extensions ||= full_paths.select { |a_path| a_path.include?('extension.where') }
+                                .map { |a_path| { url: a_path[/(?<=extension.where\(url = ').*(?='\))/] }}
+      end
+
+      # def build_extension_metadata(fhir_path)
+      #   find_url = fhir_path[/(?<=extension.where\(url = ').*/]
+
+      #   parts = find_url.split('\').', 2)
+      #   extension_metadata = {
+      #     url: parts.first
+      #   }
+      #   if parts[1].start_with?('extension')
+      #     extension_metadata[:extension] = build_extension_metadata(parts[1])
+      #   else
+      #     extension_metadata[:type] = parts[1]
+      #   end
+      #   extension_metadata
+      # end
 
       def profile_element
         @profile_element ||=
           profile_elements.find { |element| full_paths.include?(element.id) }
       end
 
-      def extension
-        extension_urls = full_paths.map { |path| path[/(?<=extension.where\(url =).*(?=\))/]}.compact
-        binding.pry unless extension_urls.empty?
-
-        @extension ||= extension_urls
-
-        @extension
+      def profile_extension
+         @profile_extension ||=
+            begin
+              ext_definition = nil
+              @extensions.each do |ext_metadata|
+                ext_definition = ig_resources.profile_by_url(ext_metadata[:url])
+                break if ext_definition.present?
+              end
+              ext_definition
+            end
       end
 
       def comparator_expectation_extensions
@@ -93,8 +133,9 @@ module USCoreTestKit
       def type
         if profile_element.present?
           profile_element.type.first.code
-        # elsif extension.present?
-        #   extension
+        elsif profile_extension.present?
+          #require 'pry'; require 'pry-byebug'; binding.pry
+          profile_extension.differential.element.find { |element| element.id == 'Extension.value[x]'}.type.first.code
         else
           # search is a variable type, eg. Condition.onsetDateTime - element
           # in profile def is Condition.onset[x]
@@ -103,7 +144,16 @@ module USCoreTestKit
       end
 
       def contains_multiple?
-        profile_element&.max == '*'
+        if profile_element.present?
+          profile_element.max == '*'
+        elsif profile_extension.present?
+          target_element = profile_elements.find do |element|
+            element.type.any? { |type| type.code == "Extension" && type.profile.include?(profile_extension.url) }
+          end
+          target_element&.max == '*'
+        else
+          false
+        end
       end
 
       def chain_extensions
