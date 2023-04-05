bc8e5780b7ec45df6c17d0db925c164dc10f8085
yunwwang <yunwwang@gmail.com>
2023-04-05 09:03:20 -0500

    Reference validation with version (#121)
    * Add intentional value set extraction to value_extractor
    
    * Move values_from_required_binding out of value_extractor
    
    * add VERSION_SPECIFIC_MESSAGE_FILTERS
    
    * attach version to target_profile in validation
    
    ---------
    
    Co-authored-by: Yunwei Wang <>

--- lib/us_core_test_kit/generator/suite_generator.rb
+++ lib/us_core_test_kit/generator/suite_generator.rb
@@ -17,6 +17,19 @@ module USCoreTestKit
         self.base_output_dir = base_output_dir
       end
 
+      def version_specific_message_filters
+        case ig_metadata.reformatted_version
+        when 'v501', 'v600_ballot'
+          [
+            # v600 ballot has a compability bug on this extension which fails patients in both v5.0.1 and v6.0.0 ballot version.
+            # TODO: remove after v600 release
+            %r{http://hl7.org/fhir/us/core/StructureDefinition/us-core-genderIdentity}
+          ]
+        else
+          []
+        end
+      end
+
       def template
         @template ||= File.read(File.join(__dir__, 'templates', 'suite.rb.erb'))
       end
