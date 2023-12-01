07fc2c212b3065f80e88bce6a4e7810c05361339
Yunwei Wang <>
2023-11-30 20:12:22 -0600

    generate first and change 1.06 to optional

---
+++ lib/us_core_test_kit/generated/v7.0.0-ballot/average_blood_pressure/average_blood_pressure_must_support_test.rb
@@ -0,0 +1,62 @@
+require_relative '../../../must_support_test'
+
+module USCoreTestKit
+  module USCoreV700_BALLOT
+    class AverageBloodPressureMustSupportTest < Inferno::Test
+      include USCoreTestKit::MustSupportTest
+
+      title 'All must support elements are provided in the Observation resources returned'
+      description %(
+        US Core Responders SHALL be capable of populating all data elements as
+        part of the query results as specified by the US Core Server Capability
+        Statement. This test will look through the Observation resources
+        found previously for the following must support elements:
+
+        * Observation.category
+        * Observation.category.coding
+        * Observation.category.coding.code
+        * Observation.category.coding.system
+        * Observation.category:VSCat
+        * Observation.code.coding
+        * Observation.code.coding.code
+        * Observation.code.coding:uscore-avg-bp
+        * Observation.component
+        * Observation.component.code
+        * Observation.component.code.coding
+        * Observation.component.code.coding.code
+        * Observation.component.valueQuantity
+        * Observation.component.valueQuantity.code
+        * Observation.component.valueQuantity.system
+        * Observation.component.valueQuantity.unit
+        * Observation.component.valueQuantity.value
+        * Observation.component:diastolic
+        * Observation.component:diastolic.code.coding:uscore-avg-diastolic-bp
+        * Observation.component:systolic
+        * Observation.component:systolic.code.coding:uscore-avg-systolic-bp
+        * Observation.effectiveDateTime
+        * Observation.effectivePeriod
+        * Observation.status
+        * Observation.subject
+        * Observation.valueQuantity
+      )
+
+      id :us_core_v700_ballot_average_blood_pressure_must_support_test
+
+      def resource_type
+        'Observation'
+      end
+
+      def self.metadata
+        @metadata ||= Generator::GroupMetadata.new(YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true))
+      end
+
+      def scratch_resources
+        scratch[:average_blood_pressure_resources] ||= {}
+      end
+
+      run do
+        perform_must_support_test(all_scratch_resources)
+      end
+    end
+  end
+end
---
+++ lib/us_core_test_kit/generated/v7.0.0-ballot/observation_clinical_result/observation_clinical_result_patient_category_date_search_test.rb
@@ -0,0 +1,48 @@
+require_relative '../../../search_test'
+require_relative '../../../generator/group_metadata'
+
+module USCoreTestKit
+  module USCoreV700_BALLOT
+    class ObservationClinicalResultPatientCategoryDateSearchTest < Inferno::Test
+      include USCoreTestKit::SearchTest
+
+      title 'Server returns valid results for Observation search by patient + category + date'
+      description %(
+A server SHALL support searching by
+patient + category + date on the Observation resource. This test
+will pass if resources are returned and match the search criteria. If
+none are returned, the test is skipped.
+
+[US Core Server CapabilityStatement](http://hl7.org/fhir/us/core//CapabilityStatement-us-core-server.html)
+
+      )
+
+      id :us_core_v700_ballot_observation_clinical_result_patient_category_date_search_test
+      input :patient_ids,
+        title: 'Patient IDs',
+        description: 'Comma separated list of patient IDs that in sum contain all MUST SUPPORT elements'
+  
+      def self.properties
+        @properties ||= SearchTestProperties.new(
+          resource_type: 'Observation',
+        search_param_names: ['patient', 'category', 'date'],
+        possible_status_search: true,
+        token_search_params: ['category'],
+        params_with_comparators: ['date']
+        )
+      end
+
+      def self.metadata
+        @metadata ||= Generator::GroupMetadata.new(YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true))
+      end
+
+      def scratch_resources
+        scratch[:observation_clinical_result_resources] ||= {}
+      end
+
+      run do
+        run_search_test
+      end
+    end
+  end
+end
