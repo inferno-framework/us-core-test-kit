634e9ebc5d60965d89206675e8fbfc552cf24af3
Stephen MacVicar <smacvicar@mitre.org>
2024-01-11 12:32:04 -0500

    fix granular scopes

--- lib/us_core_test_kit/custom_groups/smart_scopes_group.rb
+++ lib/us_core_test_kit/custom_groups/smart_scopes_group.rb
@@ -1,8 +1,7 @@
 require 'smart_app_launch_test_kit'
 require_relative '../granular_scope_checker'
 require_relative '../us_core_options'
-require_relative '../generated/v6.1.0/condition_encounter_diagnosis_group'
-require_relative '../generated/v6.1.0/condition_problems_health_concerns_group'
+require_relative '../generated/v6.1.0/condition_encounter_diagnosis/condition_encounter_diagnosis_patient_code_search_test'
 
 module USCoreTestKit
   class SmartScopesGroup < Inferno::TestGroup
@@ -12,28 +11,28 @@ module USCoreTestKit
     DEFAULT_SCOPES = 'launch/patient openid fhirUser offline_access'
 
     SMART_GRANULAR_SCOPES_GROUP1 = [
-      'patient.Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|encounter-diagnosis',
-      'patient.Condition.rs?category=http://hl7.org/fhir/us/core/CodeSystem/condition-category|health-concern',
-      'patient.DiagnosticReport.rs?category=http://loinc.org|LP29684-5',
-      'patient.DiagnosticReport.rs?category=http://loinc.org|LP29708-2',
-      'patient.DocumentReference.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-documentreference-category|clinical-note',
-      'patient.Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|laboratory',
-      'patient.Observation.rs?category=http://terminology.hl7.org//CodeSystem-observation-category.html|social-history',
-      'patient.Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|imagin',
-      'patient.Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|procedure',
-      'patient.ServiceRequest.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|sdoh'
-    ].freeze
+      'patient/Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|encounter-diagnosis',
+      'patient/Condition.rs?category=http://hl7.org/fhir/us/core/CodeSystem/condition-category|health-concern',
+      'patient/DiagnosticReport.rs?category=http://loinc.org|LP29684-5',
+      'patient/DiagnosticReport.rs?category=http://loinc.org|LP29708-2',
+      'patient/DocumentReference.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-documentreference-category|clinical-note',
+      'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|laboratory',
+      'patient/Observation.rs?category=http://terminology.hl7.org//CodeSystem-observation-category.html|social-history',
+      'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|imagin',
+      'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|procedure',
+      'patient/ServiceRequest.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|sdoh'
+    ].map(&:freeze).freeze
 
     SMART_GRANULAR_SCOPES_GROUP2 = [
-      'patient.Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|problem-list-item',
-      'patient.DiagnosticReport.rs?category=http://loinc.org|LP7839-6',
-      'patient.DiagnosticReport.rs?category=http://terminology.hl7.org/CodeSystem/v2-0074|LAB',
-      'patient.Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|vital-sign',
-      'patient.Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|treatment-intervention-preference',
-      'patient.Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|care-experience-preference',
-      'patient.ServiceRequest.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|functional-status',
-      'patient.ServiceRequest.rs?category=http://snomed.info/sct|surgical-procedure'
-    ]
+      'patient/Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|problem-list-item',
+      'patient/DiagnosticReport.rs?category=http://loinc.org|LP7839-6',
+      'patient/DiagnosticReport.rs?category=http://terminology.hl7.org/CodeSystem/v2-0074|LAB',
+      'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|vital-sign',
+      'patient/Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|treatment-intervention-preference',
+      'patient/Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|care-experience-preference',
+      'patient/ServiceRequest.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|functional-status',
+      'patient/ServiceRequest.rs?category=http://snomed.info/sct|surgical-procedure'
+    ].map(&:freeze).freeze
 
     group do
       title 'Granular Scopes 1'
@@ -66,9 +65,16 @@ module USCoreTestKit
       group do
         title 'US Core FHIR API w/Granular Scopes 1'
 
+        test from: :us_core_v610_condition_encounter_diagnosis_patient_code_search_test do
+          include GranularScopeChecker
+
+          run do
+            run_scope_check_test
+          end
+        end
         # TODO: generate all this
-        group from: :us_core_v610_condition_encounter_diagnosis
-        group from: :us_core_v610_condition_problems_health_concerns
+        # group from: :us_core_v610_condition_encounter_diagnosis
+        # group from: :us_core_v610_condition_problems_health_concerns
         # group from: :us_core_v610_diagnostic_report_lab
         # group from: :us_core_v610_diagnostic_report_note
         # group from: :us_core_v610_document_reference
@@ -83,10 +89,10 @@ module USCoreTestKit
         # # TODO: add other observation groups
         # group from: :us_core_v610_service_request
 
-        groups.each do |group|
-          group.children.select! { |test| test.id.to_s.end_with? 'search_test' }
-          group.tests.each { |test| test.include GranularScopeChecker }
-        end
+        # groups.each do |group|
+        #   group.children.select! { |test| test.id.to_s.end_with? 'search_test' }
+        #   group.tests.each { |test| test.include GranularScopeChecker }
+        # end
       end
     end
 
@@ -118,31 +124,31 @@ module USCoreTestKit
         # TODO: verify that all required scopes were requested and received
       end
 
-      group do
-        title 'US Core FHIR API w/Granular Scopes 2'
+      # group do
+      #   title 'US Core FHIR API w/Granular Scopes 2'
 
-        # TODO: generate all this
-        group from: :us_core_v610_condition_encounter_diagnosis
-        group from: :us_core_v610_condition_problems_health_concerns
-        # group from: :us_core_v610_diagnostic_report_lab
-        # group from: :us_core_v610_diagnostic_report_note
-        # group from: :us_core_v610_document_reference
-        # group from: :us_core_v610_observation_lab
-        # group from: :us_core_v610_observation_occupation
-        # group from: :us_core_v610_observation_clinical_result
-        # group from: :us_core_v610_observation_pregnancyintent
-        # group from: :us_core_v610_observation_pregnancystatus
-        # group from: :us_core_v610_observation_sexual_orientation
-        # group from: :us_core_v610_observation_screening_assessment
-        # group from: :us_core_v610_simple_observation
-        # # TODO: add other observation groups
-        # group from: :us_core_v610_service_request
+      #   # TODO: generate all this
+      #   group from: :us_core_v610_condition_encounter_diagnosis
+      #   group from: :us_core_v610_condition_problems_health_concerns
+      #   # group from: :us_core_v610_diagnostic_report_lab
+      #   # group from: :us_core_v610_diagnostic_report_note
+      #   # group from: :us_core_v610_document_reference
+      #   # group from: :us_core_v610_observation_lab
+      #   # group from: :us_core_v610_observation_occupation
+      #   # group from: :us_core_v610_observation_clinical_result
+      #   # group from: :us_core_v610_observation_pregnancyintent
+      #   # group from: :us_core_v610_observation_pregnancystatus
+      #   # group from: :us_core_v610_observation_sexual_orientation
+      #   # group from: :us_core_v610_observation_screening_assessment
+      #   # group from: :us_core_v610_simple_observation
+      #   # # TODO: add other observation groups
+      #   # group from: :us_core_v610_service_request
 
-        groups.each do |group|
-          group.children.select! { |test| test.id.to_s.end_with? 'search_test' }
-          group.tests.each { |test| test.include GranularScopeChecker }
-        end
-      end
+      #   groups.each do |group|
+      #     group.children.select! { |test| test.id.to_s.end_with? 'search_test' }
+      #     group.tests.each { |test| test.include GranularScopeChecker }
+      #   end
+      # end
     end
   end
 end
