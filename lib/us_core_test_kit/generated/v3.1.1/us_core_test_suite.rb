86bd34aebb545d95e70a36f0ad1e0474693e21e9
Dylan Hall <dehall@mitre.org>
2024-05-03 09:59:36 -0400

    Use default validator env var (#173)

--- lib/us_core_test_kit/generated/v3.1.1/us_core_test_suite.rb
+++ lib/us_core_test_kit/generated/v3.1.1/us_core_test_suite.rb
@@ -80,7 +80,6 @@ module USCoreTestKit
       id :us_core_v311
 
       fhir_resource_validator do
-        url ENV.fetch('V311_FHIR_RESOURCE_VALIDATOR_URL', 'http://hl7_validator_service:3500')
         igs 'hl7.fhir.us.core#3.1.1'
         message_filters = VALIDATION_MESSAGE_FILTERS + VERSION_SPECIFIC_MESSAGE_FILTERS
 
