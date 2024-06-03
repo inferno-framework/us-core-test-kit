723c91705c48765e97733ad880a8b647d5fe9ba6
Yunwei Wang <>
2024-06-03 11:55:25 -0500

    fix some errors

--- lib/us_core_test_kit.rb
+++ lib/us_core_test_kit.rb
@@ -2,5 +2,5 @@ require_relative 'us_core_test_kit/generated/v3.1.1/us_core_test_suite'
 require_relative 'us_core_test_kit/generated/v4.0.0/us_core_test_suite'
 require_relative 'us_core_test_kit/generated/v5.0.1/us_core_test_suite'
 require_relative 'us_core_test_kit/generated/v6.1.0/us_core_test_suite'
-require_relative 'us_core_test_kit/generated/v7.0.0-ballot/us_core_test_suite'
+require_relative 'us_core_test_kit/generated/v7.0.0/us_core_test_suite'
 
