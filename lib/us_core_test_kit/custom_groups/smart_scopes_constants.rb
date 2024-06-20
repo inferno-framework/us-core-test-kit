144373529e3e17592cd01420d4281c0c6ebf4710
Yunwei Wang <>
2024-06-20 10:05:49 -0500

    update v700 smart groups as v610

--- lib/us_core_test_kit/custom_groups/smart_scopes_constants.rb
+++ lib/us_core_test_kit/custom_groups/smart_scopes_constants.rb
@@ -10,14 +10,10 @@ module USCoreTestKit
           'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|social-history',
         ].map(&:freeze).freeze,
       'v700' => [
-          'patient/Condition.rs?category=http://hl7.org/fhir/us/core/CodeSystem/condition-category|health-concern',
-          'patient/Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|encounter-diagnosis',
-          'patient/Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|problem-list-item',
-          'patient/Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|sdoh',
-          'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|social-history',
-          'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|laboratory',
-          'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|survey',
-          'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|vital-signs'
+        'patient/Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|encounter-diagnosis',
+        'patient/Condition.rs?category=http://hl7.org/fhir/us/core/CodeSystem/condition-category|health-concern',
+        'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|laboratory',
+        'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|social-history',
         ].map(&:freeze).freeze
     }.freeze
 
@@ -29,15 +25,10 @@ module USCoreTestKit
           'patient/Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|sdoh'
       ].map(&:freeze).freeze,
       'v700' => [
-          'patient/Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|problem-list-item',
-          'patient/DiagnosticReport.rs?category=http://loinc.org|LP7839-6',
-          'patient/DiagnosticReport.rs?category=http://terminology.hl7.org/CodeSystem/v2-0074|LAB',
-          'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|vital-signs',
-          'patient/Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|treatment-intervention-preference',
-          'patient/Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|care-experience-preference',
-          'patient.Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|survey',
-          'patient/ServiceRequest.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|functional-status',
-          'patient/ServiceRequest.rs?category=http://snomed.info/sct|387713003'
+        'patient/Condition.rs?category=http://terminology.hl7.org/CodeSystem/condition-category|problem-list-item',
+        'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|vital-signs',
+        'patient/Observation.rs?category=http://terminology.hl7.org/CodeSystem/observation-category|survey',
+        'patient/Observation.rs?category=http://hl7.org/fhir/us/core/CodeSystem/us-core-category|sdoh'
         ].map(&:freeze).freeze
     }.freeze
 
