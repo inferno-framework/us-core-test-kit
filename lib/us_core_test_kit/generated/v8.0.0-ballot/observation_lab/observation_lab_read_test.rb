30ea29e12aa66f98d919d245a9219b4cccc42259
Yunwei Wang <>
2024-12-24 12:21:28 -0600

    combine pattern and value

---
+++ lib/us_core_test_kit/generated/v8.0.0-ballot/observation_lab/observation_lab_read_test.rb
@@ -0,0 +1,26 @@
+require_relative '../../../read_test'
+
+module USCoreTestKit
+  module USCoreV800_BALLOT
+    class ObservationLabReadTest < Inferno::Test
+      include USCoreTestKit::ReadTest
+
+      title 'Server returns correct Observation resource from Observation read interaction'
+      description 'A server SHALL support the Observation read interaction.'
+
+      id :us_core_v800_ballot_observation_lab_read_test
+
+      def resource_type
+        'Observation'
+      end
+
+      def scratch_resources
+        scratch[:observation_lab_resources] ||= {}
+      end
+
+      run do
+        perform_read_test(all_scratch_resources)
+      end
+    end
+  end
+end
---
+++ lib/us_core_test_kit/generated/v8.0.0-ballot/specimen/specimen_id_search_test.rb
@@ -0,0 +1,41 @@
+require_relative '../../../search_test'
+require_relative '../../../generator/group_metadata'
+
+module USCoreTestKit
+  module USCoreV800_BALLOT
+    class SpecimenIdSearchTest < Inferno::Test
+      include USCoreTestKit::SearchTest
+
+      title 'Server returns valid results for Specimen search by _id'
+      description %(
+A server SHALL support searching by
+_id on the Specimen resource. This test
+will pass if resources are returned and match the search criteria. If
+none are returned, the test is skipped.
+
+[US Core Server CapabilityStatement](/CapabilityStatement-us-core-server.html)
+
+      )
+
+      id :us_core_v800_ballot_specimen__id_search_test
+      def self.properties
+        @properties ||= SearchTestProperties.new(
+          resource_type: 'Specimen',
+        search_param_names: ['_id']
+        )
+      end
+
+      def self.metadata
+        @metadata ||= Generator::GroupMetadata.new(YAML.load_file(File.join(__dir__, 'metadata.yml'), aliases: true))
+      end
+
+      def scratch_resources
+        scratch[:specimen_resources] ||= {}
+      end
+
+      run do
+        run_search_test
+      end
+    end
+  end
+end
