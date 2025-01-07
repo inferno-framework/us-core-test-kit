800993a74806f145f800369080c813a216d2ee40
Emily Michaud <59289146+emichaud998@users.noreply.github.com>
2025-01-07 17:00:28 -0500

    Update generator metadata extractor to remove _ballot at the end of the uscore v8.0.0 id, and add generator for new interpreter required extension test

--- lib/us_core_test_kit/generator/ig_metadata_extractor.rb
+++ lib/us_core_test_kit/generator/ig_metadata_extractor.rb
@@ -21,7 +21,7 @@ module USCoreTestKit
       end
 
       def add_metadata_from_ig
-        metadata.ig_version = "v#{ig_resources.ig.version}"
+        metadata.ig_version = "v#{ig_resources.ig.version}".delete('-ballot')
       end
 
       def resources_in_capability_statement
@@ -61,17 +61,17 @@ module USCoreTestKit
 
       def remove_extra_supported_profiles
         ig_resources.capability_statement.rest.first.resource
-            .find { |resource| resource.type == 'Observation' }
-            .supportedProfile.delete_if do |profile_url|
-              SpecialCases::PROFILES_TO_EXCLUDE.include?(profile_url)
-            end
+          .find { |resource| resource.type == 'Observation' }
+          .supportedProfile.delete_if do |profile_url|
+          SpecialCases::PROFILES_TO_EXCLUDE.include?(profile_url)
+        end
       end
 
       def add_metadata_from_resources
         metadata.groups =
           resources_in_capability_statement.flat_map do |resource|
             resource.supportedProfile&.map do |supported_profile|
-              #supported_profile = supported_profile.split('|').first
+              # supported_profile = supported_profile.split('|').first
               next if supported_profile == 'http://hl7.org/fhir/uv/sdc/StructureDefinition/sdc-questionnaire'
 
               GroupMetadataExtractor.new(resource, supported_profile, metadata, ig_resources).group_metadata
--- lib/us_core_test_kit/generator/must_support_metadata_extractor_us_core_8.rb
+++ lib/us_core_test_kit/generator/must_support_metadata_extractor_us_core_8.rb
@@ -18,10 +18,17 @@ module USCoreTestKit
         @us_core_7_extractor ||= MustSupportMetadataExtractorUsCore7.new(profile, must_supports)
       end
 
+      def remove_patient_and_encounter_interpreter_extension
+        return unless profile.type == 'Patient' || profile.type == 'Encounter'
+
+        must_supports[:extensions].delete_if { |extension| extension[:url].end_with?('us-core-interpreter-needed') }
+      end
+
       def handle_special_cases
         us_core_6_extractor.add_patient_uscdi_elements
         add_must_support_choices
         us_core_6_extractor.remove_practitioner_address
+        remove_patient_and_encounter_interpreter_extension
       end
 
       def add_must_support_choices
