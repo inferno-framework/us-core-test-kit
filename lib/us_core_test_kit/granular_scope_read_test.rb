ca4b8692b58870f05ecd0a23854120cbfc402781
Yunwei Wang <>
2025-04-01 09:11:21 -0500

    update error message

--- lib/us_core_test_kit/granular_scope_read_test.rb
+++ lib/us_core_test_kit/granular_scope_read_test.rb
@@ -51,7 +51,7 @@ module USCoreTestKit
           res = nil
         end
 
-        assert (!res || res.resourceType != resource_type), "Server incorrectly returned a #{resource_type}, read should fail due to scopes"
+        assert (!res || res.resourceType == 'OperationOutcome'), "Read should fail due to scopes. Server shall return 4xx status code with an optional OperationOutcome payload."
       else
         info "Unable to find a resource that does not match scopes."
       end
