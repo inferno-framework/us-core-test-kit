f4ce031d89caee27005dd6b622b1158fc3a6ea1f
Yunwei Wang <>
2025-04-03 09:33:25 -0500

    Remove patient genderIdentity MustSupport extension from US core v6 and v7

--- lib/us_core_test_kit/generator/must_support_metadata_extractor_us_core_6.rb
+++ lib/us_core_test_kit/generator/must_support_metadata_extractor_us_core_6.rb
@@ -30,6 +30,7 @@ module USCoreTestKit
         update_smoking_status_effective
         remove_practitioner_address
         remove_diagnosticreport_media
+        remove_patient_gender_identity
       end
 
       def add_must_support_choices
@@ -105,6 +106,11 @@ module USCoreTestKit
       def remove_diagnosticreport_media
         return unless profile.id == 'us-core-diagnosticreport-note'
         must_supports[:elements].delete_if { |element| element[:path].start_with?('media') }
+      end
+
+      def remove_patient_gender_identity
+        return unless profile.type == 'Patient'
+        must_supports[:extensions].delete_if { |extension| extension[:id] == 'Patient.extension:genderIdentity' }
       end
     end
   end
