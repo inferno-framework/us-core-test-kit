fc3346a50864c51fa7b8a0b05ecfb6676699dedf
Yunwei Wang <>
2025-04-16 14:54:49 -0500

    handle exclude_from_certification

--- lib/us_core_test_kit/must_support_test.rb
+++ lib/us_core_test_kit/must_support_test.rb
@@ -11,8 +11,24 @@ module USCoreTestKit
       scratch_resources[:all]
     end
 
+    def exclude_uscdi_only_test?
+      config.options[:exclude_uscdi_only_test] == true
+    end
+
+    def must_support_extensions
+      if exclude_uscdi_only_test?
+        metadata.must_supports[:extensions].reject{ |extension| extension[:uscdi_only] }
+      else
+        metadata.must_supports[:extensions]
+      end
+    end
+
     def perform_must_support_test(resources)
       skip_if resources.blank?, "No #{resource_type} resources were found"
+
+      if (self.id.start_with?('g10_certification'))
+        metadata
+      end
 
       skip { assert_must_support_elements_present(resources, nil, metadata:) }
     end
