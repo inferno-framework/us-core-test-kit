aa84a6446924ec9aa43d3293eb6a4a3ed1a1514e
Stephen MacVicar <smacvicar@mitre.org>
2025-07-02 10:29:53 -0400

    restore constants for fhir resource navigation and primitive type

---
+++ lib/us_core_test_kit/fhir_resource_navigation.rb
@@ -0,0 +1,7 @@
+require_relative './primitive_type'
+
+module USCoreTestKit
+  # This functionality has been moved to Inferno Core, but this reference is
+  # retained to avoid a breaking change.
+  FHIRResourceNavigation = Inferno::DSL::FHIRResourceNavigation
+end
---
+++ lib/us_core_test_kit/primitive_type.rb
@@ -0,0 +1,5 @@
+module USCoreTestKit
+  # This functionality has been moved to Inferno Core, but this reference is
+  # retained to avoid a breaking change.
+  PrimitiveType = Inferno::DSL::PrimitiveType
+end
